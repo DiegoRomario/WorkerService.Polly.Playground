57b82f84be80a5f23b6c729a552add7204398a8a
Diego Romario <diego.romario@outlook.com>
2020-04-09 21:02:35 -0300

    Initial

---
+++ src/lalala.Polly.Playground/Program.cs
@@ -0,0 +1,42 @@
+﻿using Polly;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace lalala.Polly.Playground
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Demo is running!");
+            await RetryForeverPolicy();
+        }
+        static async Task CallApi()
+        {
+            Console.ResetColor();
+            Console.WriteLine("Trying to get data");
+            var httpClient = new HttpClient();
+            HttpResponseMessage result = httpClient.GetAsync("https://iholderapi.azurewebsites.net/api/v1/Tipo_investimento").Result;
+            if (result.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                var msgerro = "An error has occurred";
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(msgerro);
+                throw new Exception(msgerro);
+            }
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success in getting data");
+            await Task.CompletedTask;
+
+        }
+
+        static async Task RetryForeverPolicy()
+        {
+            var retry = Policy.Handle<Exception>().RetryForeverAsync();
+            await retry.ExecuteAsync(() => CallApi());
+        }
+
+    }
+}
